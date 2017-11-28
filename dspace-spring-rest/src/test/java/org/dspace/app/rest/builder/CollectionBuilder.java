7f859da5865507187e9e562e49249cf0ac48832c
Raf Ponsaerts <raf.ponsaerts@atmire.com>
2017-11-28 10:57:10 +0100

    [DS-3762] Wrote tests for the collection endpoint. /api/core/collections/search is not implemented yet and thus not tested

--- dspace-spring-rest/src/test/java/org/dspace/app/rest/builder/CollectionBuilder.java
+++ dspace-spring-rest/src/test/java/org/dspace/app/rest/builder/CollectionBuilder.java
@@ -7,7 +7,10 @@
  */
 package org.dspace.app.rest.builder;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.CharEncoding;
 import org.dspace.authorize.AuthorizeException;
+import org.dspace.content.Bitstream;
 import org.dspace.content.Collection;
 import org.dspace.content.Community;
 import org.dspace.content.MetadataSchema;
@@ -15,6 +18,8 @@ import org.dspace.content.service.DSpaceObjectService;
 import org.dspace.core.Context;
 import org.dspace.discovery.SearchServiceException;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.SQLException;
 
 /**
@@ -31,12 +36,23 @@ public class CollectionBuilder extends AbstractBuilder<Collection> {
         } catch (Exception e) {
             return handleException(e);
         }
-
         return this;
     }
 
     public CollectionBuilder withName(final String name) {
         return setMetadataSingleValue(collection, MetadataSchema.DC_SCHEMA, "title", null, name);
+    }
+
+    public CollectionBuilder withLogo(final String content) throws AuthorizeException, IOException, SQLException {
+
+        InputStream is = IOUtils.toInputStream(content, CharEncoding.UTF_8);
+        try {
+            collectionService.setLogo(context, collection, is);
+            return this;
+
+        } finally {
+            is.close();
+        }
     }
 
     @Override
---
+++ dspace-spring-rest/src/test/java/org/dspace/app/rest/matcher/CollectionMatcher.java
@@ -0,0 +1,31 @@
+package org.dspace.app.rest.matcher;
+
+import org.hamcrest.Matcher;
+
+import java.util.UUID;
+
+import static com.jayway.jsonpath.matchers.JsonPathMatchers.hasJsonPath;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+
+public class CollectionMatcher {
+
+    public static Matcher<? super Object> matchCollectionEntry(String name, UUID uuid, String handle) {
+        return allOf(
+                hasJsonPath("$.uuid", is(uuid.toString())),
+                hasJsonPath("$.name", is(name)),
+                hasJsonPath("$.handle", is(handle)),
+                hasJsonPath("$.type", is("collection")),
+                matchLinks(uuid)
+        );
+    }
+
+    public static Matcher<? super Object> matchLinks(UUID uuid){
+        return allOf(
+                hasJsonPath("$._links.logo.href", containsString("api/core/collections/" + uuid.toString() + "/logo")),
+                hasJsonPath("$._links.self.href", containsString("api/core/collections/" + uuid.toString()))
+        );
+    }
+
+}
